4595a565f269a4f8e0875a10b97b33e127dd0176
LordTruffle <80426487+LordTruffle@users.noreply.github.com>
2026-01-29 22:20:35 +0100

    Fix filename formatting and clean up comments

--- generate_iptv.py
+++ generate_iptv.py
@@ -8,7 +8,6 @@ from io import BytesIO
 # --- CONFIGURAZIONE ---
 OUTPUT_DIR = "playlists"
 REGIONS = ['it', 'us', 'gb', 'de', 'es', 'fr']
-# Sorgenti MJH (le più affidabili)
 SOURCES = {
     'samsung': 'https://i.mjh.nz/SamsungTVPlus/.channels.json.gz',
     'pluto': 'https://i.mjh.nz/PlutoTV/.channels.json.gz',
@@ -37,30 +36,22 @@ def run():
                 m3u_content = [f'#EXTM3U url-tvg="https://i.mjh.nz/{service.capitalize()}/{region}.xml.gz"']
                 
                 for c_id, ch in channels.items():
-                    # --- LOGICA LINK PER SERVIZIO ---
-                    
                     if service == 'pluto':
-                        # Link ottimizzato per Pluto con parametri di sessione
                         stream_url = f"https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/{c_id}/master.m3u8?advertisingId=&appName=web&appVersion=9.1.2&deviceDNT=0&deviceId={uuid.uuid4()}&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=126.0.0&sid={uuid.uuid4()}&serverSideAds=true"
-                    
                     elif service == 'samsung':
-                        # Redirect stabile per Samsung
                         slug = data.get('slug', '{id}').format(id=c_id)
                         stream_url = f"https://jmp2.uk/{slug}"
-                    
                     else: # rakuten
-                        # Link diretto per Rakuten (più stabile per i flussi m3u8)
                         stream_url = f"https://i.mjh.nz/RakutenTV/{region}/{c_id}.m3u8"
                     
-                    # Costruzione tag EXTINF
                     group = f"{service.upper()} {region.upper()}"
                     extinf = f'#EXTINF:-1 tvg-id="{c_id}" tvg-logo="{ch["logo"]}" group-title="{group}",{ch["name"]}'
                     
                     m3u_content.append(extinf)
                     m3u_content.append(stream_url)
                 
-                # Salvataggio file
-                filename = f"{service_{region}.m3u"
+                # CORRETTO: filename = f"{service}_{region}.m3u"
+                filename = f"{service}_{region}.m3u"
                 with open(os.path.join(OUTPUT_DIR, filename), "w", encoding="utf-8") as f:
                     f.write("\n".join(m3u_content))
                     
